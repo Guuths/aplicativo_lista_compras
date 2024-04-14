d40a5c038822f7c464cdf8efe62a55e2942270c9
Guuths <gustavovictal1@gmail.com>
2024-04-14 00:08:12 -0300

    alteração de cores e comentarios no sobre

--- lib/telas/telasobre.dart
+++ lib/telas/telasobre.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_lista_compras/telas/autenticacao_tela.dart';
 
 class PaginaSobre extends StatelessWidget {
-  const PaginaSobre({super.key});
+  const PaginaSobre({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sobre'),
-        backgroundColor: Color.fromARGB(255, 236, 236, 236),
+        title: const Text(
+          'Sobre',
+          style: TextStyle(
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
       ),
       backgroundColor: const Color.fromARGB(255, 70, 184, 236),
       body: Padding(
@@ -21,14 +27,23 @@ class PaginaSobre extends StatelessWidget {
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: const Text('Retornar à lista'),
+              child: const Text(
+                'Retornar à lista',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
             ),
             const SizedBox(height: 20),
-           const Text(
-              'Aplicativo desenvolvido para atuar como lista de compras. Auxiliando na hora para o individuo não esquecer nenhum item.',
+            const Text(
+              'Aplicativo desenvolvido para atuar como lista de compras. Auxiliando na hora para o indivíduo não esquecer nenhum item.',
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 53, 53, 53),
               ),
             ),
           ],
@@ -36,13 +51,33 @@ class PaginaSobre extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {return AutencicacaoTela();}),);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AutencicacaoTela()),
+          );
         },
         tooltip: 'Sair',
         child: const Icon(Icons.exit_to_app),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children:[
+              Text(
+                'Desenvolvido por Gustavo Henrique P. C. Victal',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 73, 73, 73),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
